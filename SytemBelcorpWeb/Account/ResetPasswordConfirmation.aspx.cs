fb71d92144c4c1af21a027bee9c9021f82e91cff
bdhc93 <bdhc93@gmail.com>
2015-10-01 22:27:30 -0500

    Inicio

---
+++ SytemBelcorpWeb/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace SytemBelcorpWeb.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ SytemBelcorpWeb/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SytemBelcorpWeb.Startup))]
+namespace SytemBelcorpWeb
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
